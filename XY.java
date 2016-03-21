c30f11393824acdabca771099c7f83b1ba894d0e
Dasnyl Miller <dasnyl1272007@gmail.com>
2016-03-21 01:37:42 -0700

    XYjavaprogram

---
+++ XY.java
@@ -0,0 +1,12 @@
+package com.java24hours;
+
+class XY{
+
+    public static void main(String[] arguments){
+
+         int x = 5 * 5;
+         int y = 6 * 6;
+
+        System.out.println(x + y);
+    }
+}
